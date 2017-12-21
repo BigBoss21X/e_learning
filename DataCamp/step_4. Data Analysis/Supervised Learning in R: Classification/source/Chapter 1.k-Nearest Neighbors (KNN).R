1cc537ebbd30fd41ce4bcfec3dd0f24f9156ebb6
JIHOON JUNG <jihoonjung@JIHOONs-MacBook-Air.local>
2017-12-21 15:32:53 +0900

    Dec 21, 2017, 3rd commit

--- DataCamp/step_4. Data Analysis/Supervised Learning in R: Classification/source/Chapter 1.k-Nearest Neighbors (KNN).R
+++ DataCamp/step_4. Data Analysis/Supervised Learning in R: Classification/source/Chapter 1.k-Nearest Neighbors (KNN).R
@@ -97,3 +97,69 @@ mean(signs_actual == signs_pred)
   ## Setting a k parameter allows the algorithm to consider additional nearby neighbors. This enlarges the collection of neighbors which will vote on the predicted class. 
   ## Compare k values of 1, 7, and 15 to examine the impact on traffic sign classfication accuracy. 
 
+# Instructions
+  ## The class package is already loaded in your workspace along with the datasets signs and signs_test. The object signs_actual holds the true values of the signs. 
+  # 1. Compute the accuracy of the default k = 1, model using the given code
+signs <- read.csv("data/signs")
+signs_types <- signs$sign_type
+signs_test <- signs[sample(NROW(signs), 59), ]
+
+signs_actual <- signs_test$sign_type
+
+k_1 <- knn(train = signs[-1], test = signs_test[-1], cl = signs_types)
+mean(signs_actual == k_1)
+
+k_7 <- knn(train = signs[-1], test = signs_test[-1], cl = signs_types, k = 7)
+mean(signs_actual == k_7)
+
+k_15 <- knn(train = signs[-1], test = signs_test[-1], cl = signs_types, k = 7)
+mean(signs_actual == k_15)
+
+# Chapter 7. Seeing how the neighbors voted. 
+  ## When multiple nearest neighbors hold a vote, it can sometimes be useful to examine whether the voters were unanimous or widely seperated. 
+  ## For example, knowing more about the voters' confidence in the classification could allow an autonomous vehicle to use caution in the case there is any chance at all that a stop sign is ahead. 
+  ## In this exercise, you will learn how to obtain the voting results from the knn() function. 
+
+# Instructions
+# The class package has already been loaded in your workspace along with the dataset signs. 
+  ## Build a kNN model with the prob = TRUE parameter to compute the vote proportions. Set k = 7
+  ## Use the prob parameter to get the proportion of votes for the winning class
+signs <- read.csv("data/signs")
+sign_types <- signs$sign_type
+signs_test <- signs[sample(NROW(signs), 59), ]
+
+sign_actual <- signs_test$sign_type
+
+sign_pred <- knn(train = signs[-1], test = signs_test[-1], cl = sign_types, prob = TRUE, k = 7)
+
+  ## Use the attr() function to obtain the vote proportions for the predicted class. These are stored in the attribute "prob"
+  ## Get the "prob" attribute from the predicted classes
+sign_prob <- attr(sign_pred, "prob")
+
+  ## Examine the first several vote outcomes and percentages using the head() function to see how the confidence varies from sign to sign. 
+head(sign_pred)
+head(sign_prob)
+
+# Part 3. Data prepration for kNN
+# Chapter 8. Why normalize data?
+  ## Before applying kNN to a classification task, it is common practice to rescale the data using a technique like min-max normalization. What is the purpose of this step?
+  ## Possible Answers
+# To ensure all data elements may contribute equal shares to distance
+# To help the kNN algorithm converge on a solution faster.
+# To convert all of the data elements to numbers
+# To redistribute the data as a normal bell curve
+
+## Normalization is to ensure all data elements may contribute equal shares to distance, "Rescaling" reduces the influence of extremen values on kNN's distance function. 
+
+# Normalizing data in R
+## No built-in function
+## define a min-max normalize() function
+normalize <- function(x) {
+  return((x - min(x)) / (max(x) - min(x)))
+}
+
+# normalized version of r1
+summary(normalize(signs$r1))
+
+# un-normalized version of r1
+summary(signs$r1)
