95ed423a77207bac0e70746a4005ba644dff021c
JIHOON JUNG <jihoonjung@JIHOONs-MacBook-Air.local>
2017-12-27 14:52:43 +0900

    Dec 27, 2017, 1st commit

---
+++ DataCamp/step_4. Data Analysis/Supervised Learning in R: Classification/source/Chapter 2. Naive Bayes.R
@@ -0,0 +1,11 @@
+# Estimating Probability
+# The Probability of A is denoted P(A)
+# home 10, restaurant 3, work 23, store 4
+# P(work) = 23 / 40 = 57.5%
+# P(store) = 4 / 40 = 10.0%
+
+# Joint Probability with Independent Events & Conditional Probability with Dependent Events
+
+# Making Predictions with Naive Bayes
+install.packages("naivebayes")
+library(naivebayes)
