f846bf822fbd0774bafcdba2cba0a64c797f1878
JIHOON JUNG <jihoonjung@JIHOONs-MacBook-Air.local>
2017-12-27 15:33:38 +0900

    Dec 27, 2017, 1st commit

--- DataCamp/step_4. Data Analysis/Supervised Learning in R: Classification/source/Chapter 2. Naive Bayes.R
+++ DataCamp/step_4. Data Analysis/Supervised Learning in R: Classification/source/Chapter 2. Naive Bayes.R
@@ -7,5 +7,51 @@
 # Joint Probability with Independent Events & Conditional Probability with Dependent Events
 
 # Making Predictions with Naive Bayes
-install.packages("naivebayes")
+# install.packages("naivebayes")
 library(naivebayes)
+library(tidyverse)
+
+# building a Naive Bayes Model
+url <- "https://assets.datacamp.com/production/course_2906/datasets/locations.csv"
+
+location <- read.csv(url)
+glimpse(location)
+
+where9am <- location %>% 
+  filter(hour == 9) %>% 
+  select(daytype, location) 
+
+# Chapter 1. Computing Probabilities
+# The where9am data frame contains 91 days (thirteen weeks) worth of data in which Brett recorded his location at 9am each day as well as whether the daytype was a weekend or weekday. 
+
+# Using the conditional probability formula below, you can compute the probability that Brett is working in the office, given that it is a weekday. 
+
+# P(A|B) = P(A and B) / P(B)
+
+# Calculations like these are the basic of the Naive Bayes destination prediction model you'll develop in later exercise.
+
+# Find P(office) using nrow and subset() to count rows in the dataset and save the result as p_A
+glimpse(where9am)
+?subset
+# Compute P(A)
+p_A <- nrow(subset(where9am, location == "office")) / nrow(where9am)
+
+# Find P(weekday), using nrow() and subset() again, save the result as p_B
+p_B <- nrow(subset(where9am, daytype == "weekday")) / nrow(where9am)
+
+# Use nrow() and subset() a final time to find P(office and weekday). Save the result as p_AB
+p_AB <- nrow(subset(where9am, location == "office" & daytype == "weekday")) / nrow(where9am)
+
+# Compute P(Office | weekday) and save the result as p_A_given_B
+p_A_given_B <- p_AB / p_B
+
+# Chapter 2. Understanding dependent events
+# In the previous exercise, you found that there is a 55% chance Brett is in the office at 9am given that it is a weekday. On the other hand, if Brett is never in the office on a weekend, which of the following is/are true?
+
+# Possible Answers 
+# P(office and weekend) = 0.
+# P(office | weekend) = 0.
+# Brett's location is dependent on the day of the week. 
+# All of the above. 
+
+
