daddf4edc70b976efee7c96428ac1b16fa3c38e3
JIHOON JUNG <jihoonjung@JIHOONs-MacBook-Air.local>
2018-01-06 16:21:13 +0900

    1st commit, Jan 06, 2018

--- DataCamp/step_4. Data Analysis/Supervised Learning in R: Classification/source/Chapter 3. Logistic Regression.R
+++ DataCamp/step_4. Data Analysis/Supervised Learning in R: Classification/source/Chapter 3. Logistic Regression.R
@@ -67,3 +67,77 @@ mean(donors$donated == donors$donation_pred)
 # Possible Answers
 # 80%, 85%, 90%, 95%
 # Correct! With an accuracy of only 80%, the model is actually performing WORSE than if it were to predict non-donor for every record.
+
+# Chapter 4. Calculating ROC Curves and AUC
+# The previous exercises have demonstrated that accuracy is a very misleading measure of model performance on imbalanced datasets. Graphing the model's performance better illustrates the tradeoff between a model that is overly agressive and one that is overly passive.
+# In this exercise you will create a ROC curve and compute the area under the curve (AUC) to evaluate the logistic regression model of donations you built earlier.
+
+# Instruction
+# Create a ROC curve with roc() and the columns of actual and predicted donations. Store the result as ROC.
+# Create a ROC curve
+library(pROC)
+ROC <- roc(donors$donated, donors$donation_pred)
+
+# Use plot() to draw the ROC object. Specify col = "blue" to color the curve blue.
+# Plot the ROC Curve
+plot(ROC, col = "blue")
+
+# Calculate the area under the curve(AUC)
+auc(ROC)
+
+# Awesome job! Based on this visualization, the model isn't doing much better than baseline— a model doing nothing but making predictions at random.
+
+# Let's compare, A_model is AUC = 0.55, B_model is AUC = 0.59, AUC = 0.62
+# which of the following models illustrates the best model?
+# The answer is, we need more information. 
+
+# Part 3. Dummy variables, missing data, and interactions
+# Dummy Coding categorical data
+# The most common method for handling factor data for logistic regresison
+
+# ch.1, Dummy Coding
+# Create gender factor
+# factor(factor_col, levles = c(0,1,2,...), labels = c("A", "B", "C",...))
+
+# ch.2, Imputing Missing data
+# For numeric data, average (simple strategy) but not recommended
+
+# ch.3, Interactions
+# glm(disease ~ obseity * smoking, data = health, family = "binomial")
+
+# Chapter 1. Coding categorical features
+# Sometimes a dataset contains numeric values that represent a categorical feature.
+
+# In the donors dataset, wealth_rating uses numbers to indicate the donor's wealth level:
+# 0 = Unknown
+# 1 = Low
+# 2 = Medium
+# 3 = High
+
+# This exercise illustrates how to prepare this type of categorical feature and the examines its impact on a logistic regression model.
+
+# The dataframe donors is loaded in your workspace.
+# Create a factor from the numeric wealth_rating with labels as shown above by passing the factor() function the column you want to convert, the individual levels, and the labels.
+# Convert the wealth rating to a factor
+glimpse(donors$wealth_rating)
+donors$wealth_rating <- factor(donors$wealth_rating, levels = c(0,1,2,3), labels = c("Unknown", "Low", "Medium", "High"))
+
+# relevel() takes the factor variable and the new reference level as a string.
+# Use relevel() to change reference category
+donors$wealth_rating <- relevel(donors$wealth_rating, ref = "Medium")
+
+# Nest a call to glm() inside summary().
+summary(glm(donated ~ wealth_rating, data = donors, family = "binomial"))
+
+# Chapter 2. Handling missing data
+
+# Use ifelse() and the test is.na(donor$age) to impute the average (rounded to 2 decimal places) for cases with missing age.
+round(mean(donors$age, na.rm = T), digits = 2)
+# Impute missing age values with mean(age)
+donors$imputed_age <- ifelse(is.na(donors$age), round(mean(donors$age, na.rm = T), digits = 2), donors$age)
+
+# Create a binary dummy variable named missing_age indicating the presence of missing data using another ifelse() call and the same test.
+# Create missing value indicator for age
+donors$missing_age <- ifelse(is.na(donors$age), "missing_age", donors$age)
+
+# Super! This is one way to handle missing data, but be careful! Sometimes missing data has to be dealt with using more complicated methods.
