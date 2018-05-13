fe76e2f549dc28168e5ee2002344759b0a7af185
JIHOON JUNG <jihoonjung@JIHOONs-MacBook-Air.local>
2018-05-13 22:37:53 +0900

    commit

--- DataCamp/step_4. Data Analysis/Statistical Modeling/source/Chapter 1. Statistical Modeling.R
+++ DataCamp/step_4. Data Analysis/Statistical Modeling/source/Chapter 1. Statistical Modeling.R
@@ -70,7 +70,7 @@ install_pkgs <- function(pkg) {
   sapply(pkg, require, character.only = TRUE)
 }
 
-pkgs <- c("mosaicData", "mosaicModel", "ggplot2", "dplyr", "tidyr", "readr", "purrr", "tibble", "stringr", "forcats", "mosaic")
+pkgs <- c("mosaicData", "mosaicModel", "ggplot2", "dplyr", "tidyr", "readr", "purrr", "tibble", "stringr", "forcats", "mosaic", "lattice")
 
 install_pkgs(pkgs)
 
@@ -111,3 +111,49 @@ library(ggplot2)
 
 # Look at the head() of diamonds
 head(diamonds)
+
+#### Chapter 3. Starting with formulas #### 
+# Formulas such as wage ~ age + exper are used to describe the form of relationships among variables. In this exercise, you are going to use formulas and functions to summarize data on the cost of life insurance policies. The data are in the AARP data frame, which has been preloaded from the statisticalModeling package.
+
+# The mosaic package augments simple statistical functions such as mean(), sd(), median(), etc. so that they can be used with formulas. For instance, mosaic::mean(wage ~ sex, data = CPS85) will calculate the mean wage for each sex. In contrast, the "built-in" mean() function (part of the base package) doesn't accept formulas, making it unnecessarily hard to do things like calculate groupwise means.
+
+# Note that we explicitly reference the mean() function from the mosaic package using double-colon notation (i.e. package::function()) to make it clear that we're not using the base R version of mean(). If you'd like, you can watch a supplemental video here to learn more about formulas and functions in R.
+
+#### ~ Instructions ####
+
+# Find the variable names in the AARP data frame, which has been preloaded in your workspace from the statisticalModeling package. AARP contains life insurance prices for the two sexes at different ages.
+
+# Construct a formula using the variable names from the AARP data frame with an eye toward calculating the insurance cost broken down by sex. Use that formula as the first argument to mosaic::mean() to find the mean cost by sex.
+
+# Find the variable names in AARP
+data("AARP")
+names(AARP)
+
+# Find the mean cost broken down by sex
+mosaic::mean(Cost ~ Sex, data = AARP)
+
+### Chapter 4. Graphics with forumals #### 
+
+# Formulas can be used to describe graphics in each of the three popular graphics systems: base graphics, lattice graphics, and in ggplot2 with the statisticalModeling package. Most people choose to work in one of the graphics systems. I recommend ggplot2 with the formula interface provided by statisticalModeling.
+
+#### ~ Instructions ####
+# 1. Make a boxplot of insurance cost broken down by sex in one of the three graphics systems. All can use the same syntax: function(formula, data = AARP).
+
+# 2. In base graphics, the appropriate function is boxplot().
+boxplot(Cost ~ Age, data = AARP)
+
+# 3. In lattice graphics, use bwplot() to make the boxplot. As always, you will have to load the lattice package.
+bwplot(Cost ~ Age, data = AARP)
+
+# 4. In the formula interface to ggplot2, use gf_boxplot(). The statisticalModeling package (which communicates with ggplot2), has been loaded for you.
+gf_boxplot(Cost ~ Age, data = AARP)
+
+# 5. Make a scatterplot of Cost versus Age in one of the graphics packages.
+
+# 6. In base graphics: plot().
+plot(Cost ~ Age, data = AARP)
+# 7. In lattice graphics: xyplot().
+xyplot(Cost ~ Age, data = AARP)
+
+# 8. For ggplot graphics: gf_point().
+gf_point(Cost ~ Age, data = AARP)
