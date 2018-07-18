4375aa19681c05ad01a21a554456f582aa87ac66
JIHOON JUNG <jihoonjung@JIHOONs-MacBook-Air.local>
2018-07-18 09:03:24 +0900

    commit

--- DataCamp/step_4. Data Analysis/Analyzing Survey Data in R/Chapter 1. Introduction to survey data/source/Chapter 1. Introduction to Survey Data.R
+++ DataCamp/step_4. Data Analysis/Analyzing Survey Data in R/Chapter 1. Introduction to survey data/source/Chapter 1. Introduction to Survey Data.R
@@ -100,3 +100,60 @@ strat_design <- svydesign(data = apistrat, weights = ~pw, fpc = ~fpc, id = ~1, s
 
 # # Look at the summary information for the stratified design
 summary(strat_design)
+
+#### ~ Chapter 4. Cluster Designs in R ####
+
+# Now let's practice specifying a cluster sampling design, using the dataset apiclus2. The schools were clustered based on school districts, dnum. Within a sampled school district, 5 schools were randomly selected for the sample. The schools are denoted by snum. The number of districts is given by fpc1 and the number of schools in the sampled districts is given by fpc2.
+
+# Instructions 
+# (1) Glimpse the data
+# Glimpse the data
+glimpse(apiclus2)
+
+# (2) Specify the design for apiclus2 and store in an object called apiclus_design
+# Specify the design
+apiclus_design <- svydesign(id = ~dnum + snum, data = apiclus2, weights = ~pw, fpc = ~fpc1 + fpc2)
+
+# (3) Look at the summary information for the design.
+#Look at the summary information stored in the design object
+summary(apiclus_design)
+
+# Good work! Now let's compare the survey weights of these three designs.
+
+#### ~ Chapter 5. Comparing survey weights of different designs 
+# Remember that an observation's survey weight tells us how many population units that observation represents. The weights are constructed based on the sampling design. Let's compare the weights for the three samples of the api dataset. For example, in simple random sampling, each unit has an equal chance of being sampled, so each observation gets an equal survey weight. Whereas, for stratified and cluster sampling, units have an unequal chance of being sampled and that is reflected in the survey weight.
+
+# Instructions 
+# (1) Using ggplot2, construct a histogram of the survey weights, pw, for the simple random sample, apisrs.
+# Construct histogram of pw
+ggplot(data = apisrs,
+       mapping = aes(x = pw)) + 
+  geom_histogram()
+
+# (2) Now construct a histogram of the survey weights, pw, for the stratified sample, apistrat. 
+# Construct histogram of pw
+ggplot(data = apistrat,
+       mapping = aes(x = pw)) + 
+  geom_histogram()
+
+# (3) Lastly, construct a histogram of the survey weights, pw, for the cluster sample, apiclus2. Compare the histograms!
+# Construct histogram of pw
+ggplot(data = apiclus2,
+       mapping = aes(x = pw)) + 
+  geom_histogram()
+
+# Now that we can specify designs, let's start doing some analyses!
+
+#### Lecture 3. Visualizing the impact of survey weights ####
+
+# install.packages("NHANES", dependencies = TRUE)
+library(NHANES)
+dim(NHANESraw)
+library(dplyr)
+summarise(NHANESraw, N_hat = sum(WTMEC2YR))
+
+NHANESraw <- mutate(NHANESraw, WTMEC4YR = WTMEC2YR/2)
+
+NHANES_design <- svydesign(NHANESraw, strata = ~SDMVSTRA, id = ~SDMVPSU, nest = TRUE, weights = ~WTMEC4YR)
+
+glimpse(NHANESraw)
